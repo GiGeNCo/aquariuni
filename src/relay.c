9cc2f602d50de508fd72fbbe552bc41d53b58385
Sun Dro <kala0x13@gmail.com>
2015-06-29 19:30:24 +0400

    final changes with relay driver

--- src/relay.c
+++ src/relay.c
@@ -10,25 +10,6 @@
 #include "stdinc.h"
 #include "relay.h"
 
-
-/* 
- * init_relay - function initializes sain smart 8 
- * relay module and makes pins ready for digital write 
- */
-void init_relay(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8)
-{
-    /* Initialize pins */
-    if (p1) pinMode(p1, OUTPUT);
-    if (p2) pinMode(p2, OUTPUT);
-    if (p3) pinMode(p3, OUTPUT);
-    if (p4) pinMode(p4, OUTPUT);
-    if (p5) pinMode(p5, OUTPUT);
-    if (p6) pinMode(p6, OUTPUT);
-    if (p7) pinMode(p7, OUTPUT);
-    if (p8) pinMode(p8, OUTPUT);
-}
-
-
 /* 
  * open_relay - Function opens relay. 
  * Argument is pin which we want to open.
@@ -41,3 +22,21 @@ void open_relay(int pin) { digitalWrite(pin, 0); }
  * Argument is pin which we want to close.
  */
 void close_relay(int pin) { digitalWrite(pin, 1); }
+
+
+/* 
+ * init_relay - function initializes sain smart 8 
+ * relay module and makes pins ready for digital write 
+ */
+void init_relay(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8)
+{
+    /* Initialize pins */
+    if (p1) { pinMode(p1, OUTPUT); close_relay(p1); }
+    if (p2) { pinMode(p2, OUTPUT); close_relay(p2); }
+    if (p3) { pinMode(p3, OUTPUT); close_relay(p3); }
+    if (p4) { pinMode(p4, OUTPUT); close_relay(p4); }
+    if (p5) { pinMode(p5, OUTPUT); close_relay(p5); }
+    if (p6) { pinMode(p6, OUTPUT); close_relay(p6); }
+    if (p7) { pinMode(p7, OUTPUT); close_relay(p7); }
+    if (p8) { pinMode(p8, OUTPUT); close_relay(p8); }
+}
